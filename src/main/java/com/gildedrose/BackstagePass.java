28d86f1e6839e70533fb9b38c138625a0cedffad
Kristian Jakobsen <kristian@nymann.dev>
2023-04-12 22:22:59 +0200

    Refactor: Replace conditionals with polymorphism

---
+++ src/main/java/com/gildedrose/BackstagePass.java
@@ -0,0 +1,29 @@
+package com.gildedrose;
+
+public class BackstagePass extends Item {
+    public BackstagePass(int sellIn, int quality) {
+        super("Backstage passes to a TAFKAL80ETC concert", sellIn, quality);
+    }
+
+    @Override
+    protected void doUpdateQuality() {
+        if (quality < 50) {
+            quality = quality + 1;
+
+            if (sellIn < 11 && quality < 50) {
+                quality = quality + 1;
+            }
+
+            if (sellIn < 6 && quality < 50) {
+                quality = quality + 1;
+            }
+        }
+
+        sellIn = sellIn - 1;
+
+        if (sellIn < 0) {
+            quality = 0;
+        }
+
+    }
+}
--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -7,57 +7,9 @@ class GildedRose {
         this.items = items;
     }
 
-    private static void doUpdateQuality(Item item) {
-        switch (item.name) {
-            case "Aged Brie":
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-
-                item.sellIn = item.sellIn - 1;
-
-                if (item.sellIn < 0 && item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
-                break;
-            case "Sulfuras, Hand of Ragnaros":
-                return;
-            case "Backstage passes to a TAFKAL80ETC concert":
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if (item.sellIn < 11 && item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-
-                    if (item.sellIn < 6 && item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-
-                item.sellIn = item.sellIn - 1;
-
-                if (item.sellIn < 0) {
-                    item.quality = 0;
-                }
-                break;
-            default:
-                if (item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-
-                item.sellIn = item.sellIn - 1;
-
-                if (item.sellIn < 0 && item.quality > 0) {
-                    item.quality = item.quality - 1;
-                }
-                break;
-        }
-    }
-
     public void updateQuality() {
         for (Item item : items) {
-            doUpdateQuality(item);
+            item.doUpdateQuality();
         }
     }
 
